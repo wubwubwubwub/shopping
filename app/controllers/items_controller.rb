ec2c387f3962bfa39dfa33b6524024f14bd01c49
wubwubwubwub <nolan.camp@gmail.com>
2014-08-21 23:23:05 -0400

    add delete to index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,13 @@ class ItemsController < ApplicationController
     redirect_to :back
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    
+    redirect_to :back
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :list_id, :quan, :notes)
